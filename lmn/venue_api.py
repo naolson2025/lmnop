d536892196c71550b4ad08e28a38fd5503304fb7
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-11-07 18:58:45 -0600

    change to save the data by the events

--- lmn/venue_api.py
+++ lmn/venue_api.py
@@ -1,11 +1,14 @@
 import requests
 
-url = 'https://app.ticketmaster.com/discovery/v2/venues.json?apikey=8yFkVOfTiN4HSZQNcjNKWhJ1OTlmYWJh'
-params = {'statecode' : 'MN', 'type' : 'venue'}
+url = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=8yFkVOfTiN4HSZQNcjNKWhJ1OTlmYWJh'
+params = {'dmaId' : '336', 'classificationName' : 'music'}
 data = requests.get(url, params=params).json()
 
-venue_data = data['_embedded']['venues']
+events_data = data['_embedded']['venues']
 
-for data in venue_data:
+for data in events_data:
     name = data['name']
-    print(f'Venue: {name}')
+    venue_name = data['_embedded']['venues'][0]['name']
+    print(f'Event: {name}')
+    print(f'Venue: {venue_name}')
+

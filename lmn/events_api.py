668d940a85d4718eccd1c73d85bdb273e2188093
Clara <10088152+claraj@users.noreply.github.com>
2019-11-07 20:30:23 -0600

    Update events_api.py
    Also move this file to somewhere outside the web app code.

--- lmn/events_api.py
+++ lmn/events_api.py
@@ -9,11 +9,16 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-container = soup.find_all('a')
-                               
-for lines in container:
-    title = lines.title
-    print(title)
+articles = soup.findAll('article')
 
-    
-    
+for article in articles: 
+    links = article.find_all(class_ = "even")
+    for l in links:
+        print(l.text)
+
+""" So you could examine all the l.text strings. 
+The first one is the show, unless it ends with 'presents' in which case it's the second one. 
+The venue is the l.text that starts with 'at' """
+
+
+

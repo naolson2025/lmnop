b8cb197adec3c32141c4d3fd1fd2f72bc0ee9a06
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-11-07 19:01:08 -0600

    Edit the data request

--- lmn/venue_api.py
+++ lmn/venue_api.py
@@ -4,11 +4,11 @@ url = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=8yFkVOfTiN4H
 params = {'dmaId' : '336', 'classificationName' : 'music'}
 data = requests.get(url, params=params).json()
 
-events_data = data['_embedded']['venues']
+events_data = data['_embedded']['events']
 
 for data in events_data:
     name = data['name']
     venue_name = data['_embedded']['venues'][0]['name']
     print(f'Event: {name}')
-    print(f'Venue: {venue_name}')
+    print(f'Venue: {venue_name} \n')
 

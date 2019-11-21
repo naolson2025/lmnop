0cd8cf7477411341b43f1bea9b00007d03d0fd32
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-11-21 01:07:09 -0600

    Adding test events data exists

---
+++ lmn/tests/test_webscrapers.py
@@ -0,0 +1,35 @@
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from django.test import TestCase
+import unittest
+
+# https://www.tutorialspoint.com/python_web_scraping/python_web_scraping_testing_with_scrapers.htm
+
+class TestEventsData(unittest.TestCase):
+
+    bs = None
+
+    def setUpClass():
+        url = "https://www.bandsintown.com/en?place_id=ChIJvbt3k5Azs1IRB-56L4TJn5M"
+        TestEventsData.bs = BeautifulSoup(urlopen(url), 'html.parser')
+    
+    def test_data_exists(self):
+        artist_data = TestEventsData.bs.find_all('h2', {'class' : 'artist-data'})
+        venue_data = TestEventsData.bs.find_all('div', {'class' : 'venue-data'})
+        city_state_data = TestEventsData.bs.find_all('div', {'class' : 'city-and-state-data'})
+        date_data = TestEventsData.bs.find_all('div', {'class' : 'date-data'})
+
+        self.assertIsNotNone(artist_data)
+        self.assertIsNotNone(venue_data)
+        self.assertIsNotNone(city_state_data)
+        self.assertIsNotNone(date_data)
+
+
+
+
+
+    
+
+
+
